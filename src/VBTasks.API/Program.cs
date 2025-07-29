a9fa96ead1808f0194c93e86048491ac18fd53ce
Ale <alejandrocuellar18@gmail.com>
2025-07-29 00:38:07 -0400

    fix enum

--- src/VBTasks.API/Program.cs
+++ src/VBTasks.API/Program.cs
@@ -8,7 +8,11 @@ using VBTasks.Infrastructure.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
